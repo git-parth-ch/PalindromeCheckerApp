c50216280aff43850117b208a5df6113dca43cd9
git-parth-ch <parthchhabra713@gmail.com>
2026-03-03 09:09:23 +0530

    Base application setup

---
+++ src/PalindromeApp.java
@@ -0,0 +1,7 @@
+public class PalindromeApp{
+    public static void main (String[]args){
+        System.out.println("Welcome to the Palindrome Checker Management System");//Welcome Message
+        System.out.println("Version 1.0");
+        System.out.println("System Initialised successfully");
+    }
+}
